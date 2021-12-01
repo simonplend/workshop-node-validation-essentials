f173f5e7044e7fb130fbe85f3e1cb2676262407a
Simon Plenderleith <simon@simonplend.co.uk>
2021-12-01 15:31:03 +0000

    WIP

---
+++ workshop/01-why-validation-is-essential/completed/routes.exercise-1.1.completed.js
@@ -0,0 +1,33 @@
+import { db } from "../database.js";
+
+export const routes = [
+  {
+    method: "POST",
+    path: "/recipes",
+    handler: async function (request, response) {
+      const recipe = request.body;
+
+      if (
+        /** Check the `recipe` object has an `ingredients` property. */
+        !recipe.hasOwnProperty("ingredients") ||
+        /** Check that the `ingredients` property is an array. */
+        !Array.isArray(recipe.ingredients) ||
+        /** Check that the `ingredients` array has 1 or more items. */
+        !recipe.ingredients.length >= 1
+      ) {
+        /** `return` early to stop the route handler function continuing. */
+        return;
+      }
+
+      const newRecipe = await db.insertRecipe(recipe);
+
+      console.log(`Recipe ingredients:`, recipe.ingredients.join(", "));
+
+      response.statusCode = 201;
+
+      response.setHeader("Content-Type", "application/json");
+
+      response.write(JSON.stringify(newRecipe));
+    },
+  },
+];
---
+++ workshop/01-why-validation-is-essential/completed/routes.exercise-1.2.completed.js
@@ -0,0 +1,36 @@
+import { db } from "../database.js";
+
+export const routes = [
+  {
+    method: "POST",
+    path: "/recipes",
+    handler: async function (request, response) {
+      const recipe = request.body;
+
+      if (
+        !recipe.hasOwnProperty("ingredients") ||
+        !Array.isArray(recipe.ingredients) ||
+        !recipe.ingredients.length >= 1
+      ) {
+        /**
+         * Find the appropriate 4×× Client Error status code (422 — Unprocessable Entity).
+         *
+         * Set the response HTTP status code.
+         */
+        response.statusCode = 422;
+
+        return;
+      }
+
+      const newRecipe = await db.insertRecipe(recipe);
+
+      console.log(`Recipe ingredients:`, recipe.ingredients.join(", "));
+
+      response.statusCode = 201;
+
+      response.setHeader("Content-Type", "application/json");
+
+      response.write(JSON.stringify(newRecipe));
+    },
+  },
+];
